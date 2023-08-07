3450a9bdeb45d2fbda6ad1140ff278871ea13e01
hieunghia <nghianguyenhieu@vnpt.vn>
2023-08-07 21:29:19 +0700

    push

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table = 'nguoi_quan_tri';
     protected $fillable = [
         'username',
         'firstname',
